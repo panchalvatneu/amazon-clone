556f5280b2e5dc14f65c5bdebd657211af3ba484
Viraj Panchal <91445953+panchalvatneu@users.noreply.github.com>
2024-09-16 20:43:59 -0700

    Initiation

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+        className="home__image"
+        src="https://m.media-amazon.com/images/I/61lXretRKRL._SX3000_.jpg"
+        alt="latest deal on alexa" 
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
